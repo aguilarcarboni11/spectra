311d6e6453e170ce116bc61a78b31ae05ba582ad
aguilarcarboni <aguilarcarboni@gmail.com>
2023-10-11 12:31:09 -0600

    Add leaflet map

--- src/components/routes/spectra/components/map/SpectraMap.js
+++ src/components/routes/spectra/components/map/SpectraMap.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import { useFetch } from '../../../../hooks/useFetch'
-import { Map, Marker, Overlay, ZoomControl } from 'pigeon-maps'
-import { osm } from 'pigeon-maps/providers'
+
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet';
+
 import { spectraState } from '../../../../misc/types/types.tsx'
 
 import SpectraGraph from '../misc/SpectraGraph'
@@ -10,13 +13,13 @@ import ClearButton from '../misc/ClearButton'
 
 const SpectraMap = ({state, setState, formulario, setFormulario}) => {
 
+    const icon = L.icon({ iconUrl: './assets/marker.png' });
+
     const [center, setCenter] = useState([9.7489, -83.7535])
     const [currentHover, setHover] = useState(null)
 
     const dataFetch = useFetch()
     const coordsFetch = useFetch()
-
-    const markerColor = '#FF914D'
 
     useEffect(() => {
         dataFetch.Post(`SELECT * from "Formulario"`)
@@ -59,39 +62,66 @@ const SpectraMap = ({state, setState, formulario, setFormulario}) => {
         }
     }
 
+    const myIcon = L.divIcon({
+        className: 'marker',
+        iconAnchor: [15,30],
+        html: `<i class="bi bi-geo-alt-fill"></i>`
+    });
+
   return (
     <div className='mapContainer'>
-        <div className='head'>
+        <div className='header'>
             <p className='subtitle'> {state === spectraState.FORMULARIO ? `Formulario #${formulario['ID']}`:'De click en un punto para ver más informacion acerca del mismo'} </p>
             <ClearButton clearSelection={clearSelection} state={state}/>
         </div>
         <div className='mapAndInfo'>
-            <div className='map'>
-                <Map
-                    height={'100%'}
-                    width={'100%'}
-                    provider={osm}
-                    center = {center}
-                    defaultZoom={7} 
-                    minZoom={7}>
-                    {dataFetch.data.length !== 0 ? dataFetch.data.map((element, index) => 
-                        <Marker key = {index} width={'2vw'} anchor={[parseFloat(element.Latitud),parseFloat(element.Longitud)]} onMouseOver={() => handleHoverIn([element['Punto']])} onMouseOut={() => handleHoverOut()} onClick = {() => selectMarker(element)} color = {markerColor}></Marker>
-                    ):''}
-                    {coordsFetch.data.length !== 0 ? coordsFetch.data.map((coord, index) => 
-                        <Overlay key = {index} className = {(ArrayCompare(currentHover, coord)) ? 'overlay':'overlay hidden'} anchor={coord} offset={[50,-5]} >
-                            <p className='subtitle'> {coord[0]} , {coord[1]} </p>
-                        </Overlay>
-                    ):''}
-                    <ZoomControl style = {{top: '1vh', left: '1vw'}}/>
-                </Map>
+            <div id='map'>
+                <MapContainer 
+                    center={center} 
+                    zoom={8} 
+                    minZoom={8}
+                    scrollWheelZoom={true}
+                    className='full-height-map'
+                >
+                    <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker className='marker' position={[9.9377, -84.1429]} icon={myIcon}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                    <Marker className='marker' position={center} icon={myIcon}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+                {/* 
+                    <Map
+                        minZoom={7}
+                        center={center} 
+                    >
+                        {dataFetch.data.length !== 0 ? dataFetch.data.map((element, index) => 
+                            <Marker key = {index} width={'2vw'} anchor={[parseFloat(element.Latitud),parseFloat(element.Longitud)]} onMouseOver={() => handleHoverIn([element['Punto']])} onMouseOut={() => handleHoverOut()} onClick = {() => selectMarker(element)} color = {markerColor}></Marker>
+                        ):''}
+                        {coordsFetch.data.length !== 0 ? coordsFetch.data.map((coord, index) => 
+                            <Overlay key = {index} className = {(ArrayCompare(currentHover, coord)) ? 'overlay':'overlay hidden'} anchor={coord} offset={[50,-5]} >
+                                <p className='subtitle'> {coord[0]} , {coord[1]} </p>
+                            </Overlay>
+                        ):''}
+                        <ZoomControl style = {{top: '1vh', left: '1vw'}}/>
+                    </Map> 
+                */}
             </div>
             {state === spectraState.FORMULARIO ? 
-            <div className={state === spectraState.FORMULARIO ? 'info': 'info hidden'}> 
-                {dataFetch.data.length !== 0 ? Object.keys(dataFetch.data[0]).map((element, index) => 
-                    <p key = {index} className='subtitle'>{formulario[element]}</p>
-                ):''}
-                <SpectraGraph height={'40%'} width={'90%'}/>
-            </div> : ''
+                <div className={state === spectraState.FORMULARIO ? 'info': 'info hidden'}> 
+                    {dataFetch.data.length !== 0 ? Object.keys(dataFetch.data[0]).map((element, index) => 
+                        <p key = {index} className='subtitle'>{formulario[element]}</p>
+                    ):''}
+                    <SpectraGraph height={'40%'} width={'90%'}/>
+                </div> : ''
             }
         </div>
      </div>
