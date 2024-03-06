b4fcec98ff6be99e75d37922bf12513d68bfc1e1
aguilarcarboni <aguilarcarboni@gmail.com>
2024-03-06 13:29:26 -0600

    Push

---
+++ src/components/routes/homepage/components/Carrousel.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react'
+import ArrowButton from '../../../misc/ArrowButton'
+
+const Carrousel = () => {
+  const [index,setIndex] = useState(0)
+  const carrouselPhotos = []
+  //const images = require('../../../../assets/fotos/fotos-prias/')
+
+  for (var i = 1; i < 15; i++) {
+    var string = i.toString()
+    carrouselPhotos.push({path: string.concat('.JPG'), key: i})
+  }
+
+  const nextSlide = () => {
+    if (index !== carrouselPhotos.length-2) {
+      setIndex(index+1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  const prevSlide = () => {
+    if (index === 0) {
+      setIndex(carrouselPhotos.length-2);
+    } else {
+      setIndex(index - 1);
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide()
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className='carrouselContainer'>
+      <div className='carrousel'>
+        <div className='photosContainer'>
+        {carrouselPhotos.slice(index,index+2).map((image, count) => (
+          <div className='carrouselPhotoContainer' key={count}>
+            <img src={require("../../../../assets/fotos/fotos-prias/" + image.path)} alt={'Diapositiva' + image.key} className='image'></img>
+          </div>
+        ))}
+        </div>
+      </div>
+      <div className='textContainer'>
+        <ArrowButton id = 'left' onClick={prevSlide}/>
+        <p className='subtitle'> Biblioteca de fotos del PRIAS</p>
+        <ArrowButton id = 'right' onClick={nextSlide} height={2} width={2}/>
+      </div>
+    </div>
+  )
+}
+
+export default Carrousel
